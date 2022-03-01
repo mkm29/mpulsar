75dbbd7c1827a488fe12febae1dcb84950cbffc7
Mitch Murphy Murphy <mitchell.murphy@spathesystems.com>
2022-03-01 17:38:06 -0500

    Set up basic structure of Pulsar client; added an HTTP server to receive and post message to Pulsar server.

--- cmd/mpulsar/main.go
+++ cmd/mpulsar/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 
@@ -11,32 +12,172 @@ import (
 
 // declare constants for Pulsar
 const (
-	server     = "54.211.97.53"
-	port       = 6650
-	topicName1 = "my-topic-1"
-	topicName2 = "my-topic-2"
+	server = "54.211.97.53"
+	port   = 6650
+	topic  = "users"
 )
 
-func main() {
-	// Hello world, the web server
+var (
+	client   pulsar.Client
+	producer pulsar.Producer
+	consumer pulsar.Consumer
+	reader   pulsar.Reader
+)
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Example Pulsar Client in Go!\n")
-	}
-
-	http.HandleFunc("/hello", helloHandler)
-	log.Println("Listing for requests at http://localhost:8000/hello")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+// create struct to hold message
+type Message struct {
+	ID   pulsar.MessageID
+	Text string
 }
 
-func createClient() {
+func main() {
+	// initialize Pulsar
+	initPulsar()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/message/create", messageCreate)
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
+
+// initialize all Pulsar variables
+func initPulsar() {
+	// create client
+	client, err := createClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	// create producer
+	producer, err := createProducer(client, "my-topic")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer producer.Close()
+	// create consumer
+	consumer, err := createConsumer(client, "my-topic", "my-sub")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer consumer.Close()
+	// create reader
+	// create ReaderOptions
+	ro := pulsar.ReaderOptions{
+		Topic: topic,
+		Name:  "my-reader",
+		Properties: map[string]string{
+			"key1": "value1",
+		},
+		StartMessageID:          pulsar.EarliestMessageID(),
+		StartMessageIDInclusive: true,
+		// MessageChannel:          make(chan pulsar.ConsumerMessage),
+		ReceiverQueueSize: 10,
+	}
+	reader, err := createReader(client, ro)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+}
+
+func messageCreate(w http.ResponseWriter, r *http.Request) {
+	// Declare a new Person struct.
+	var m Message
+
+	// Try to decode the request body into the struct. If there is an error,
+	// respond to the client with the error message and a 400 status code.
+	err := json.NewDecoder(r.Body).Decode(&m)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Produce message to Pulsar
+	msgId, err := sendMessage(producer, m.Text)
+	_ = msgId
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(w, "Message: %+v", m)
+
+}
+
+func createClient() (pulsar.Client, error) {
 	client, err := pulsar.NewClient(pulsar.ClientOptions{
 		URL: fmt.Sprintf("pulsar://%s:%d", server, port),
+	})
+
+	return client, err
+}
+
+func createConsumer(client pulsar.Client, t string, s string) (pulsar.Consumer, error) {
+	consumer, err := client.Subscribe(pulsar.ConsumerOptions{
+		Topic:            t,
+		SubscriptionName: s,
+		Type:             pulsar.Shared,
+	})
+
+	return consumer, err
+}
+
+func createProducer(client pulsar.Client, t string) (pulsar.Producer, error) {
+	producer, err := client.CreateProducer(pulsar.ProducerOptions{
+		Topic: t,
+	})
+	return producer, err
+}
+
+func sendMessage(producer pulsar.Producer, msg string) (pulsar.MessageID, error) {
+	ctx := context.Background()
+
+	msgId, err := producer.Send(ctx, &pulsar.ProducerMessage{
+		Payload: []byte(msg),
 	})
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer client.Close()
+	return msgId, err
+}
+
+func consumeMessage(consumer pulsar.Consumer) pulsar.Message {
+	ctx := context.Background()
+
+	msg, err := consumer.Receive(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	consumer.Ack(msg)
+
+	return msg
+}
+
+func createReader(client pulsar.Client, ro pulsar.ReaderOptions) (pulsar.Reader, error) {
+	/*
+	 * Create a Reader with ReaderOptions
+	 *
+	 * ReaderOptions are optional and can be used to configure the Reader
+	 *
+	 * For example, you can set the Reader's name, schema, and schema type
+	 *
+	 * For more information on ReaderOptions, see:
+	 * https://godoc.org/github.com/apache/pulsar-client-go/pulsar#ReaderOptions
+	 */
+	reader, err := client.CreateReader(pulsar.ReaderOptions{
+		Topic:                   ro.Topic,
+		Name:                    ro.Name,
+		Properties:              ro.Properties,
+		StartMessageID:          ro.StartMessageID,
+		StartMessageIDInclusive: ro.StartMessageIDInclusive,
+		MessageChannel:          ro.MessageChannel,
+		ReceiverQueueSize:       ro.ReceiverQueueSize,
+		SubscriptionRolePrefix:  ro.SubscriptionRolePrefix,
+		Decryption:              ro.Decryption,
+	})
+	return reader, err
 }
